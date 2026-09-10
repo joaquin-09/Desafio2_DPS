34bf444ce1ed8699dd98292621c434aa989d5982
joaquin-09 <quini170804@gmail.com>
2026-09-10 14:44:53 -0600

    agregar: index.ts de types

---
+++ desafio2/src/types/index.ts
@@ -0,0 +1,29 @@
+export type Categoria = 'alimento' | 'bebida';
+
+export interface Producto {
+  id: string;
+  nombre: string;
+  precio: number;
+  imagen: string;
+  categoria: Categoria;
+}
+
+export interface ItemOrden {
+  producto: Producto;
+  cantidad: number;
+}
+
+export interface ItemHistorial {
+  nombre: string;
+  cantidad: number;
+  precioUnitario: number;
+}
+
+export interface OrdenHistorial {
+  id: string;
+  fecha: string; // ISO 8601
+  items: ItemHistorial[];
+  subtotal: number;
+  iva: number;
+  total: number;
+}
