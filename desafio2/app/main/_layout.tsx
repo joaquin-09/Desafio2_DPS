b41858faa159e8775ca68c6d4317213d75363cb0
joaquin-09 <quini170804@gmail.com>
2026-09-10 16:10:33 -0600

    agregando: logica de cerrar sesion con los usuarios correspondientes

--- desafio2/app/main/_layout.tsx
+++ desafio2/app/main/_layout.tsx
@@ -1,10 +1,12 @@
 import { Text, TouchableOpacity, Alert } from 'react-native';
 import { Tabs, useRouter } from 'expo-router';
 import { useOrden } from '../../src/context/OrdenContext';
+import { useAuth } from '../../src/context/AuthContext';
 
 export default function MainLayout() {
   const router = useRouter();
   const { limpiarOrden } = useOrden();
+  const { cerrarSesion: cerrarSesionAuth, usuarioActual } = useAuth();
 
   const cerrarSesion = () => {
     Alert.alert('Cerrar sesión', '¿Seguro que quieres salir?', [
@@ -14,6 +16,7 @@ export default function MainLayout() {
         style: 'destructive',
         onPress: () => {
           limpiarOrden();
+          cerrarSesionAuth();
           router.replace('/');
         },
       },
