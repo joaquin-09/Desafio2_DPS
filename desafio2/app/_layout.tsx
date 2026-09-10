1114cc799f5d223f4b22a69fdf09ecec2ae5d3e1
joaquin-09 <quini170804@gmail.com>
2026-09-10 16:07:57 -0600

    arreglar: el contenido de _layout.tsx

--- desafio2/app/_layout.tsx
+++ desafio2/app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from 'expo-router';
+import { AuthProvider } from '../src/context/AuthContext';
 import { OrdenProvider } from '../src/context/OrdenContext';
 
 export default function RootLayout() {
   return (
-    <OrdenProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="main" />
-      </Stack>
-    </OrdenProvider>
+    <AuthProvider>
+      <OrdenProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="main" />
+        </Stack>
+      </OrdenProvider>
+    </AuthProvider>
   );
 }
