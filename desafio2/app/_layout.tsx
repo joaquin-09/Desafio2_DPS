2c647b994579eddc4f4c74c7a69749924b1fb45d
joaquin-09 <quini170804@gmail.com>
2026-09-10 15:19:23 -0600

    agregar contenido de _layout.tsx de app

---
+++ desafio2/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import { OrdenProvider } from '../src/context/OrdenContext';
+
+export default function RootLayout() {
+  return (
+    <OrdenProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(main)" />
+      </Stack>
+    </OrdenProvider>
+  );
+}
